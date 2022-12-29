51eed3153dd9024a97552b31e29dff1182b6ff06
truelanz <devalanoliveira@gmail.com>
2022-12-29 11:25:42 -0300

    calcule o metro quadrado

--- src/edu/truelanz/variaveis/TiposVariaveis1.java
+++ src/edu/truelanz/variaveis/TiposVariaveis1.java
@@ -1,10 +1,29 @@
 package edu.truelanz.variaveis;
 
+import java.util.Scanner;
+
 public class TiposVariaveis1{
 
     public static void main(String [] args){
 
+        //CALCULE QUAL O M2 (METRO QUADRADO) DE UM DETERMINADO LOCAL.
+
+        Scanner in = new Scanner(System.in);
+
+        double x = 0;
+        double y = 0;
         
+        System.out.println("Digite o comprimento: ");
+        x = in.nextDouble();
+        
+        System.out.println("Digite a largura: ");
+        y = in.nextDouble();
+        
+        double m2 = x*y;
+
+        System.out.println(m2 + "m2");
+
+
 
 
     }
