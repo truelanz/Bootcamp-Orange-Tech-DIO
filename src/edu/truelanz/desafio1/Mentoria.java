9d4b372199e0623761fd22524f629759cf66be6d
truelanz <devalanoliveira@gmail.com>
2023-01-17 12:16:43 -0300

    mentoria e curso

---
+++ src/edu/truelanz/desafio1/Mentoria.java
@@ -0,0 +1,38 @@
+package edu.truelanz.desafio1;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo, descricao;
+    private LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+    }
+
+    
+    
+}
