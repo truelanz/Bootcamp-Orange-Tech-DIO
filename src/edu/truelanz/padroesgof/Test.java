fe7487fbb6cea4df532214b7b7624e9ea4e1361a
truelanz <devalanoliveira@gmail.com>
2023-01-30 12:22:48 -0300

    Padroes de Projetos -  Singleton

---
+++ src/edu/truelanz/padroesgof/Test.java
@@ -0,0 +1,25 @@
+package edu.truelanz.padroesgof;
+
+public class Test {
+
+    // Testes Design Pattern Singleton;
+    public static void main(String[] args) {
+        
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonHolder holder = SingletonHolder.getInstancia();
+        System.out.println(holder);
+        holder = SingletonHolder.getInstancia();
+        System.out.println(holder);
+    }
+}
+    
+
